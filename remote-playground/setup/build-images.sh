79b76033ee2e0e4f8bf977f9779bd7440f995a5a
Daniel Sokolowski <github@d.sokolowski.org>
2018-09-27 12:46:01 +0530

    Regenerate test server keys only if not already existing

--- remote-playground/setup/build-images.sh
+++ remote-playground/setup/build-images.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Generate server key
-yes y | ssh-keygen -t rsa -b 4096 -N "" -C "i3ql-test-server" -q -f test-server.key
-cp test-server.key* test-server
-cp test-server.key* test-client
+if [ ! -f test-server.key ] || [ ! -f test-server.key.pub ]; then
+	yes y | ssh-keygen -t rsa -b 4096 -N "" -C "i3ql-test-server" -q -f test-server.key
+	cp test-server.key* test-server
+	cp test-server.key* test-client
+fi
 
 # Build the images
 docker build -t i3ql-test-server test-server
