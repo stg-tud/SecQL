c522323afd7e02b3eedb5ea7fbda3dafde2abc4b
Daniel Sokolowski <github@d.sokolowski.org>
2018-10-02 14:50:13 +0200

    Moved out company benchmark to own project

--- project/Build.scala
+++ project/Build.scala
@@ -4,80 +4,77 @@ import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
 
 object sae extends Build {
 
-    /*
-    Project IDB
-  */
+	/*
+	Project IDB
+  	*/
 	lazy val idb = Project(id = "idb", base = file("idb"))
-		.aggregate (runtime, intermediateRepresentation, schemaExamples, runtimeCompiler, syntax, integrationTest)
+		.aggregate(runtime, intermediateRepresentation, schemaExamples, runtimeCompiler, syntax, integrationTest)
 
 	lazy val runtime = Project(id = "idb-runtime", base = file("idb/runtime"))
 
-    lazy val intermediateRepresentation = Project(id = "idb-intermediate-representation", base = file("idb/intermediate-representation"))
+	lazy val intermediateRepresentation = Project(id = "idb-intermediate-representation", base = file("idb/intermediate-representation"))
 
 	lazy val schemaExamples = Project(id = "idb-schema-examples", base = file("idb/schema-examples"))
 
 	lazy val runtimeCompiler = Project(id = "idb-runtime-compiler", base = file("idb/runtime-compiler"))
-		.dependsOn (schemaExamples % "compile;test", runtime % "compile;test", intermediateRepresentation % "compile;test")
+		.dependsOn(schemaExamples % "compile;test", runtime % "compile;test", intermediateRepresentation % "compile;test")
 
 	lazy val syntax = Project(id = "idb-syntax-iql", base = file("idb/syntax-iql"))
-		.dependsOn (runtimeCompiler % "compile;test", schemaExamples % "compile;test", runtime % "compile;test")
+		.dependsOn(runtimeCompiler % "compile;test", schemaExamples % "compile;test", runtime % "compile;test")
 
 	lazy val integrationTest = Project(id = "idb-integration-test", base = file("idb/integration-test"))
-		.dependsOn (schemaExamples % "test", syntax % "test", intermediateRepresentation % "test")
-		
-
-  /*
-      Project Remote playground
-     */
-    lazy val remotePlayground = Project(id = "remote-playground", base = file("remote-playground"))
-            .dependsOn(syntax % "compile;test").dependsOn(runtime % "compile;test").dependsOn(schemaExamples % "compile;test").dependsOn(intermediateRepresentation % "compile;test")
-            .settings(multiJvmSettings: _*)
-          .configs (MultiJvm)
+		.dependsOn(schemaExamples % "test", syntax % "test", intermediateRepresentation % "test")
 
 
-    /*
-      Project Distributed Benchmarks
-     */
-    lazy val distributedBenchmarks = Project(id = "distributed-benchmarks", base = file("distributed-benchmarks"))
-            .dependsOn(
-                syntax,
-                runtime,
-                schemaExamples,
-                intermediateRepresentation
-            )
+	/*
+	Project Remote playground
+	*/
+	lazy val remotePlayground = Project(id = "remote-playground", base = file("remote-playground"))
+		.dependsOn(syntax % "compile;test").dependsOn(runtime % "compile;test").dependsOn(schemaExamples % "compile;test").dependsOn(intermediateRepresentation % "compile;test")
+		.settings(multiJvmSettings: _*)
+		.configs(MultiJvm)
 
-    import com.typesafe.sbt.SbtMultiJvm.multiJvmSettings
-    import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
 
-    lazy val hospitalBenchmark = Project(id = "hospital-benchmark", base = file("distributed-benchmarks/hospital"))
-            .dependsOn(
-                distributedBenchmarks
-            )
-            .settings(
-                distributedBenchmarkSettings,
-                multiJvmSettings
-            )
-            .configs(MultiJvm)
+	/*
+	Project Distributed Benchmarks
+	*/
+	lazy val distributedBenchmarks = Project(id = "distributed-benchmarks", base = file("distributed-benchmarks"))
+		.dependsOn(
+			syntax,
+			runtime,
+			schemaExamples,
+			intermediateRepresentation
+		)
 
-    lazy val distributedBenchmarkSettings = Seq(
-    )
+	import com.typesafe.sbt.SbtMultiJvm.multiJvmSettings
+	import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
 
-    /*
-      Project Test Data
-    */
+	lazy val companyBenchmark = Project(id = "company-benchmark", base = file("distributed-benchmarks/company"))
+		.dependsOn(distributedBenchmarks)
+		.settings(multiJvmSettings)
+		.configs(MultiJvm)
 
-    lazy val testData = Project(id = "test-data", base = file("test-data"))
+	lazy val hospitalBenchmark = Project(id = "hospital-benchmark", base = file("distributed-benchmarks/hospital"))
+		.dependsOn(distributedBenchmarks)
+		.settings(multiJvmSettings)
+		.configs(MultiJvm)
 
-    /*
-    Root Project
-  */  
-  lazy val root = Project(id = "sae", base = file("."))
-		.aggregate (runtime, intermediateRepresentation, schemaExamples, runtimeCompiler, syntax, integrationTest, testData, remotePlayground)
-  
+	/*
+	Project Test Data
+	*/
 
-  val virtScala = Option(System.getenv("SCALA_VIRTUALIZED_VERSION")).getOrElse("2.11.2")
+	lazy val testData = Project(id = "test-data", base = file("test-data"))
 
-    val akkaVersion = "2.4.4"
+	/*
+	Root Project
+	*/
+	lazy val root = Project(id = "sae", base = file("."))
+		.aggregate(runtime, intermediateRepresentation, schemaExamples, runtimeCompiler, syntax, integrationTest, testData, remotePlayground)
 
-    val lmsVersion = "latest.integration"
+
+	val virtScala = Option(System.getenv("SCALA_VIRTUALIZED_VERSION")).getOrElse("2.11.2")
+
+	val akkaVersion = "2.4.4"
+
+	val lmsVersion = "latest.integration"
 }
