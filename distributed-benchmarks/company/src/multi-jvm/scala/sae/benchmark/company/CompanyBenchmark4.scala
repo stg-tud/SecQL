4284cf243df9e65f31d8c6a65cbb5c8501285e65
Daniel Sokolowski <github@d.sokolowski.org>
2018-11-14 15:03:11 +0530

    Added latency recording to CompanyBenchmark4

--- distributed-benchmarks/company/src/multi-jvm/scala/sae/benchmark/company/CompanyBenchmark4.scala
+++ distributed-benchmarks/company/src/multi-jvm/scala/sae/benchmark/company/CompanyBenchmark4.scala
@@ -1,12 +1,11 @@
 package sae.benchmark.company
 
 import akka.remote.testkit.MultiNodeSpec
-import idb.{Relation, algebra}
-import idb.algebra.print.RelationalAlgebraPrintPlan
-import idb.query.taint._
-import idb.query.{QueryEnvironment, RemoteHost}
-import idb.syntax.iql.IR
+import idb.Relation
+import idb.observer.NoOpObserver
+import idb.query.QueryEnvironment
 import sae.benchmark.BenchmarkMultiNodeSpec
+import sae.benchmark.company.CompanyMultiNodeConfig._
 
 class CompanyBenchmark4MultiJvmNode1 extends CompanyBenchmark4
 class CompanyBenchmark4MultiJvmNode2 extends CompanyBenchmark4
@@ -23,17 +22,9 @@ class CompanyBenchmark4 extends MultiNodeSpec(CompanyMultiNodeConfig)
 	//Specifies the number of measurements/warmups
 	with DefaultPriorityConfig {
 
-	override val benchmarkQuery = "4"
-
-	import CompanyMultiNodeConfig._
+	override val benchmarkQuery = "query4"
 
 	//Setup query environment
-	val publicHost = RemoteHost("public-host", node(rolePublic))
-	val productionHost = RemoteHost("production-host", node(roleProduction))
-	val purchasingHost = RemoteHost("purchasing-host", node(rolePurchasing))
-	val employeesHost = RemoteHost("employees-host", node(roleEmployees))
-	val clientHost = RemoteHost("clients-host", node(roleClient))
-
 	implicit val env = QueryEnvironment.create(
 		system,
 		Map(
@@ -45,77 +36,122 @@ class CompanyBenchmark4 extends MultiNodeSpec(CompanyMultiNodeConfig)
 		)
 	)
 
-	def internalBarrier(name : String): Unit = {
-		enterBarrier(name)
+	type ResultType = (Int, Int)
+
+	object PublicDBNode extends PublicDBNode
+
+	object ProductionDBNode extends ProductionDBNode {
+		override protected def addComponentHook(componentId: Int): Unit = {
+			if (componentId % 10 == 4)
+				logLatency(componentId / 10, "query")
+		}
 	}
 
-	type ResultType = (Long, Long, Int, Int)
+	object PurchasingDBNode extends PurchasingDBNode {
+		override protected def addSupplierHook(supplierId: Int): Unit = {
+			if (supplierId % 10 == 4) {
+				logLatency(supplierId / 10, "query")
+				logLatency((iterations + supplierId) / 10, "query")
+			}
+		}
+
+		override protected def addSCHook(supplierId: Int, componentId: Int): Unit = {
+			if (componentId % 10 == 4)
+				logLatency(componentId / 10, "query")
+		}
+	}
+
+	object EmployeesDBNode extends EmployeesDBNode
 
 	object ClientNode extends ReceiveNode[ResultType]("client") {
 		override def relation(): Relation[ResultType] = {
 			//Write an i3ql query...
 			import BaseCompany._
+			import idb.schema.company._
 			import idb.syntax.iql.IR._
 			import idb.syntax.iql._
-			import idb.schema.company._
 
-			val products : Rep[Query[Product]] = RECLASS (REMOTE GET (publicHost, "product-db"), labelPublic)
-			val factories : Rep[Query[Factory]] = RECLASS (REMOTE GET (publicHost, "factory-db"), labelPublic)
+			val products: Rep[Query[Product]] = RECLASS(REMOTE GET(publicHost, "product-db"), labelPublic)
+			val factories: Rep[Query[Factory]] = RECLASS(REMOTE GET(publicHost, "factory-db"), labelPublic)
 
-			val components : Rep[Query[Component]] = RECLASS (REMOTE GET (productionHost, "component-db"), labelProduction)
-			val pcs : Rep[Query[PC]] = RECLASS (REMOTE GET (productionHost, "pc-db"), labelProduction)
-			val fps : Rep[Query[FP]] = RECLASS (REMOTE GET (productionHost, "fp-db"), labelProduction)
+			val components: Rep[Query[Component]] = RECLASS(REMOTE GET(productionHost, "component-db"), labelProduction)
+			val pcs: Rep[Query[PC]] = RECLASS(REMOTE GET(productionHost, "pc-db"), labelProduction)
+			val fps: Rep[Query[FP]] = RECLASS(REMOTE GET(productionHost, "fp-db"), labelProduction)
 
-			val suppliers : Rep[Query[Supplier]] = RECLASS (REMOTE GET (purchasingHost, "supplier-db"), labelPurchasing)
-			val scs : Rep[Query[SC]] = RECLASS (REMOTE GET (purchasingHost, "sc-db"), labelPurchasing)
+			val suppliers: Rep[Query[Supplier]] = RECLASS(REMOTE GET(purchasingHost, "supplier-db"), labelPurchasing)
+			val scs: Rep[Query[SC]] = RECLASS(REMOTE GET(purchasingHost, "sc-db"), labelPurchasing)
 
-			val employees : Rep[Query[Employee]] = RECLASS (REMOTE GET (employeesHost, "employee-db"), labelEmployees)
-			val fes : Rep[Query[FE]] = RECLASS (REMOTE GET (employeesHost, "fe-db"), labelEmployees)
+			val employees: Rep[Query[Employee]] = RECLASS(REMOTE GET(employeesHost, "employee-db"), labelEmployees)
+			val fes: Rep[Query[FE]] = RECLASS(REMOTE GET(employeesHost, "fe-db"), labelEmployees)
 
-//			val q =
-//				SELECT ((c : Rep[Component], s : Rep[Supplier], sc : Rep[SC]) =>
-//					(c.timestamp, s.timestamp, c.id, s.id)
-//				) FROM (
-//					components, suppliers, scs
-//				) WHERE ((c, s, sc) =>
-//					sc.price < 60.00 AND sc.inventory >= 4 AND c.id == sc.componentId AND
-//						c.material == "Wood" AND s.id == sc.supplierId AND
-//						(s.city == "Darmstadt" OR s.city == "Hanau") AND
-//						s.name.startsWith("Bauhaus")
-//				)
+			//			val q =
+			//				SELECT ((c : Rep[Component], s : Rep[Supplier], sc : Rep[SC]) =>
+			//					(c.id, s.id)
+			//				) FROM (
+			//					components, suppliers, scs
+			//				) WHERE ((c, s, sc) =>
+			//					sc.price < 60.00 AND sc.inventory >= 4 AND c.id == sc.componentId AND
+			//						c.material == "Wood" AND s.id == sc.supplierId AND
+			//						(s.city == "Darmstadt" OR s.city == "Hanau") AND
+			//						s.name.startsWith("Bauhaus")
+			//				)
 
+			// Matches iteration == supplierId, (iteration || iterations + iteration) == componentId
+			// if iteration % 10 == (3 || 4)
 			val qSC =
-				SELECT (*) FROM scs WHERE (sc =>
-					sc.price < 60.00 AND sc.inventory >= 4
-				)
+			SELECT(*) FROM scs WHERE (sc =>
+				sc.price < 60.00 AND sc.inventory >= 4)
 
+			// Matches iteration == supplierId if iteration % 10 == (4 || 6) => 2  * iterations / 10 = #suppliers
 			val qSupplier =
-				SELECT (*) FROM suppliers WHERE (s =>
-					(s.city == "Darmstadt" OR s.city == "Hanau") AND s.name.startsWith("Bauhaus")
-				)
+				SELECT(*
+				) FROM suppliers WHERE (s =>
+					(s.city == "Darmstadt" OR s.city == "Hanau") AND s.name.startsWith("Bauhaus"))
 
-			val qa =
-				SELECT ((s : Rep[Supplier], sc : Rep[SC]) =>
-					(s.timestamp, s.id, sc.componentId)
-				) FROM (qSupplier, qSC) WHERE ((s, sc) =>
-					s.id == sc.supplierId
-				)
+			// ==> 2 scs per supplier => 2 * iterations / 10 = #scs
+			val qSupplierComponent =
+				SELECT((s: Rep[Supplier], sc: Rep[SC]) =>
+					(s.id, sc.componentId)
+				) FROM(
+					qSupplier, qSC
+				) WHERE ((s, sc) =>
+					s.id == sc.supplierId)
 
-			val q =
-				SELECT ((c : Rep[Component], e : Rep[(Long, Int, Int)]) =>
-					(c.timestamp, e._1, c.id, e._3)
-				) FROM (components, qa) WHERE ((c : Rep[Component], e : Rep[(Long, Int, Int)]) =>
-					c.id == e._3 AND c.material == "Wood"
-				)
-
-
-			//Compile to LMS representation (only needed for printing)
-			val query : Rep[Query[ResultType]] = q
+			// All components contain wood: 2 * iterations / 10 = #results
+			val query4 =
+				SELECT((c: Rep[Component], sc: Rep[(Int, Int)]) =>
+					(c.id, sc._1)
+				) FROM(
+					components, qSupplierComponent
+				) WHERE ((c: Rep[Component], sc: Rep[(Int, Int)]) =>
+					c.id == sc._2 AND c.material == "Wood")
 
 			//Define the root. The operators get distributed here.
-			val r : idb.Relation[ResultType] =
-			ROOT(clientHost, query)
+			val r: idb.Relation[ResultType] =
+				ROOT(clientHost, query4)
+
+			// Setup latency recording
+			r.addObserver(new NoOpObserver[ResultType] {
+				override def added(v: (Int, Int)): Unit = {
+					// Component / 10 id is used as latency id
+					logLatency(v._1 / 10, "query", false)
+				}
+
+				override def addedAll(vs: Seq[(Int, Int)]): Unit = {
+					vs.foreach(v => added(v))
+				}
+			})
+
 			r
+		}
+
+		override protected def sleepUntilCold(expectedEvents: Int = 0): Unit = {
+			try {
+				super.sleepUntilCold(2 * iterations / 10)
+			}
+			catch {
+				case e: IllegalArgumentException => log.error(e.getMessage + "\nIgnored for this test case, since it tends to produce a few duplicates")
+			}
 		}
 	}
 
