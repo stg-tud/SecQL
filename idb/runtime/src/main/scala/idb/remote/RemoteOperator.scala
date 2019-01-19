732ec14a6e8092dfb6e18dce25a6e3488789d136
Daniel Sokolowski <github@d.sokolowski.org>
2019-01-19 11:59:37 +0100

    Improved remote operator initialization

--- idb/runtime/src/main/scala/idb/remote/RemoteOperator.scala
+++ idb/runtime/src/main/scala/idb/remote/RemoteOperator.scala
@@ -30,7 +30,7 @@ class RemoteOperator[Domain](val relation: Relation[Domain])
 			initializeOperator(relation)
 			sender ! Initialize
 		case SetupStream =>
-			sender ! Await.result(initializeStream(), timeout.duration)
+			sender ! Await.result(streamRef(), timeout.duration)
 		case Reset =>
 			relation.reset()
 			sender ! ResetCompleted
@@ -58,13 +58,12 @@ class RemoteOperator[Domain](val relation: Relation[Domain])
 
 	protected def rootRelation: Relation[Domain] = relation
 
-	protected def initializeStream(): Future[SourceRef[DataMessage[Domain]]] = {
-		var stream: Source[DataMessage[Domain], _] = null
-
+	protected lazy val stream: Source[DataMessage[Domain], _] = {
 		if (remoteConnections.isEmpty) {
 			// This operator is a leaf in the tree and therefore a data source itself
 			log.info(s"Initializing leaf remote operator of $relation")
-			stream = relation match {
+
+			relation match {
 				// If already a publisher, pull based back pressure can be ensured without loss
 				case view: RemotePublisher[Domain] =>
 					Source.fromPublisher(view)
@@ -77,7 +76,8 @@ class RemoteOperator[Domain](val relation: Relation[Domain])
 			val remoteConnection = remoteConnections(0)
 			val remoteSource = remoteConnection.source
 			log.info(s"Initializing unary remote operator of $relation")
-			stream = remoteSource
+
+			remoteSource
 				.mapConcat(StreamAdapter.wrapLinearOperatorTree(remoteConnection, rootRelation))
 		}
 		else {
@@ -85,10 +85,12 @@ class RemoteOperator[Domain](val relation: Relation[Domain])
 			val firstSource = remoteConnections(0).source
 			val secondSource = remoteConnections(1).source
 			val otherSources = remoteConnections.slice(2, remoteConnections.size).map(_.source)
-			stream = Source.combine(firstSource, secondSource, otherSources: _*)(WrapOperatorTree(remoteConnections, rootRelation))
-		}
 
-		stream.runWith(StreamRefs.sourceRef())
+			Source.combine(firstSource, secondSource, otherSources: _*)(WrapOperatorTree(remoteConnections, rootRelation))
+		}
 	}
+
+	protected def streamRef(): Future[SourceRef[DataMessage[Domain]]] =
+		stream.runWith(StreamRefs.sourceRef())
 
 }
